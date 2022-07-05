a9eb366681b66fa4d259edd801a98ef3940098fd
dannypellatt <danielpellatt92@gmail.com>
2022-07-05 13:01:33 -0400

    initial commit

---
+++ MethodsExercise2/Methods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace MethodsExercise2
+{
+    public class Methods
+    {
+        public static int Add(params int[] values)
+        {
+            int sum = 0;
+            foreach (var number in values)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        public static int Subtract(params int[] values)
+        {
+            int sum = 0;
+            foreach (var number in values)
+            {
+                sum -= number;
+            }
+            return sum;
+        }
+
+        public static int Multiply(params int[] values)
+        {
+            int sum = 1;
+            foreach (var number in values)
+            {
+                sum *= number;
+            }
+            return sum;
+        }
+
+        public static int Divide(params int[] values)
+        {
+            int sum = values[0];
+            foreach (var number in values [1..])
+            {
+                sum /= number;
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using MethodsExercise2;
+
+Console.WriteLine(Methods.Add(6, 10, 8, 42));
+Console.WriteLine(Methods.Subtract(6, 10, 8, 42));
+Console.WriteLine(Methods.Multiply(6, 10, 8, 42));
+Console.WriteLine(Methods.Divide(20, 2, 2));
+
